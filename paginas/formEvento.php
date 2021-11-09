ad42d774123c120ca07c9039a264c3f8229faa50
ChristianLoo <christianloo0812@gmail.com>
2021-11-09 11:07:05 -0500

    commit

--- paginas/formEvento.php
+++ paginas/formEvento.php
@@ -93,79 +93,9 @@
 										placeholder="" title="Facultad de la universidad"/>
 										<br><br>
 										<label class="textoCuerpoWhite">Periodo cubierto:</label><!--name hora1-->
-										<select name="hora1">
-											<option value="7">7</option>
-											<option value="8">8</option>
-											<option value="9">9</option>
-											<option value="10">10</option>
-											<option value="11">11</option>
-											<option value="12">12</option>
-											<option value="13">13</option>
-											<option value="14">14</option>
-											<option value="15">15</option>
-											<option value="16">16</option>
-											<option value="17">17</option>
-											<option value="18">18</option>
-										</select>
-										<select name="minuto1"><!--name minuto1-->
-											<option value="00">00</option>
-											<option value="01">01</option>
-											<option value="02">02</option>
-											<option value="03">03</option>
-											<option value="04">04</option>
-											<option value="05">05</option>
-											<option value="06">06</option>
-											<option value="07">07</option>
-											<option value="08">08</option>
-											<option value="09">09</option>
-											<option value="07">07</option>
-											<option value="10">10</option>
-											<option value="11">11</option>
-											<option value="12">12</option>
-											<option value="13">13</option>
-											<option value="14">14</option>
-											<option value="15">15</option>
-											<option value="16">16</option>
-											<option value="17">17</option>
-											<option value="18">18</option>
-										</select>
+										<input type="time" id="horaActividad" class="txtbox" placeholder="" name="horaActividad">
 										<label class="textoCuerpoWhite">a</label>
-										<select name="hora2"><!--name hora2-->
-											<option value="7">7</option>
-											<option value="8">8</option>
-											<option value="9">9</option>
-											<option value="10">10</option>
-											<option value="11">11</option>
-											<option value="12">12</option>
-											<option value="13">13</option>
-											<option value="14">14</option>
-											<option value="15">15</option>
-											<option value="16">16</option>
-											<option value="17">17</option>
-											<option value="18">18</option>
-										</select>
-										<select name="minuto2"><!--name minuto2-->
-											<option value="00">00</option>
-											<option value="01">01</option>
-											<option value="02">02</option>
-											<option value="03">03</option>
-											<option value="04">04</option>
-											<option value="05">05</option>
-											<option value="06">06</option>
-											<option value="07">07</option>
-											<option value="08">08</option>
-											<option value="09">09</option>
-											<option value="07">07</option>
-											<option value="10">10</option>
-											<option value="11">11</option>
-											<option value="12">12</option>
-											<option value="13">13</option>
-											<option value="14">14</option>
-											<option value="15">15</option>
-											<option value="16">16</option>
-											<option value="17">17</option>
-											<option value="18">18</option>
-										</select>
+										<input type="time" id="horaActividad" class="txtbox" placeholder="" name="horaActividad">
 										<br><br>
 										<label class="textoCuerpoWhite">Lugar donde se realizará:</label>  <select name="lugar"><!--name lugar-->
 										<?php while ($lugares=$sql2->fetch(PDO::FETCH_OBJ)) { ?>

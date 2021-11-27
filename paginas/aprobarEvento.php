280d4e6f8fcd222bc7de5022b96781ac0c6211c8
ChristianLoo <christianloo0812@gmail.com>
2021-11-27 00:29:12 -0500

    commit

--- paginas/aprobarEvento.php
+++ paginas/aprobarEvento.php
@@ -79,7 +79,7 @@
                     </table>
                 </div>
                 <div>
-                    <a href="menuPrincipal.php"><button class="redondo" type="submit"><label class="textoCuerpo">REGRESAR</label></button></a>
+                    <a href="menuPrincipal.php"><button class="redondo espacioBoton5" type="submit"><label class="textoCuerpo">REGRESAR</label></button></a>
                 <div>
                 <br><br>
 	    </div>
